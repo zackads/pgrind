80d8e2a4fdbcbcf2cc65dd55cca0ae4f3a649b80
Zack Adlington <zackads@users.noreply.github.com>
2025-03-29 20:39:13 +0000

    Stop tracking __pycache__/

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,22 @@
-FROM ubuntu:latest
-LABEL authors="zack"
+FROM python:3.12-slim AS builder
+RUN mkdir /app
+WORKDIR .app
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+RUN pip install --upgrade pip
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
-ENTRYPOINT ["top", "-b"]
+FROM python:3.13-slim
+RUN useradd -m -r appuser && \
+    mkdir /app && \
+    chown -R appuser /app
+COPY --from=builder /usr/local/lib/python3.13/site-packages/ /usr/local/lib/python3.13/site-packages/
+COPY --from=builder /usr/local/bin/ /usr/local/bin/
+WORKDIR /app
+COPY --chown=appuser:appuser . .
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+USER appuser
+EXPOSE 8000
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "--workers", "3", "my_docker_django_app.wsgi:application"]
--- pgrind/settings.py
+++ pgrind/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l1#91f4p$to(^vyr2a82=esi)6iq3@@7-_^fl*)8x=z3*9c(pq"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "paper_questions.apps.PaperQuestionsConfig"
+    "paper_questions.apps.PaperQuestionsConfig",
 ]
 
 MIDDLEWARE = [
