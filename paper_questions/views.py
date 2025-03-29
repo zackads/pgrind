d91a79aff619343bb11131ac6dac0e56467f5658
Zack Adlington <zackads@users.noreply.github.com>
2025-03-29 19:59:51 +0000

    Add TailwindUI styling
    # Conflicts:
    #	.gitignore

--- paper_questions/views.py
+++ paper_questions/views.py
@@ -3,37 +3,38 @@ import random
 from django.shortcuts import render
 from django.templatetags.static import static
 
+
 def index(request):
     """Home"""
-    return render(request, 'paper_questions/index.html')
+    return render(request, "paper_questions/index.html")
+
 
 def question(request):
-    subject = request.GET.get('subject') or 'comp_arch'
-    id = request.GET.get('id') or random.randint(1, 5)
+    subject = request.GET.get("subject") or "comp_arch"
+    id = request.GET.get("id") or random.randint(1, 5)
 
     return render(
         request,
-        'paper_questions/question.html',
+        "paper_questions/question.html",
         {
-            'subject': subject,
-            'id': id,
-            'image_url': static(f"paper_questions/{subject}-q-{id}.png"),
-            'solution_url': f'/solution?subject={subject}&id={id}'
-        })
+            "subject": subject,
+            "id": id,
+            "image_url": static(f"paper_questions/{subject}-q-{id}.png"),
+            "solution_url": f"/solution?subject={subject}&id={id}",
+        },
+    )
+
 
 def solution(request):
-    subject = request.GET.get('subject')
-    id = request.GET.get('id') or random.randint(1, 5)
+    subject = request.GET.get("subject")
+    id = request.GET.get("id") or random.randint(1, 5)
 
     return render(
         request,
-        'paper_questions/solution.html',
+        "paper_questions/solution.html",
         {
-            'subject': subject,
-            'id': id,
-            'image_url': static(f"paper_questions/{subject}-s-{id}.png"),
-            'question_url': f'/question?subject={subject}&id={id}',
-            'same_subject_question_url': f'/question?subject={subject}',
-            'random_subject_question_url': f'/question',
-        }
+            "image_url": static(f"paper_questions/{subject}-s-{id}.png"),
+            "question_url": f"/?subject={subject}&id={id}",
+            "next_url": "/",
+        },
     )
